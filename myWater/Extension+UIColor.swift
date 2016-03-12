43a2ea1759c73b8ae9d9e30c7716fe22bbbd5133
Marc Felden <marc.felden@me.com>
2016-03-12 21:42:13 +0100

    test1

---
+++ myWater/Extension+UIColor.swift
@@ -0,0 +1,17 @@
+//
+//  Extension+UIColor.swift
+//  ToDo
+//
+//  Created by Marc Felden on 12.03.16.
+//  Copyright © 2016 Dominik Hauser. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    
+    class func myWaterLightBlue() -> UIColor {
+        return UIColor(red: 218/255, green: 233/255, blue: 241/255, alpha: 1.0)
+    }
+}
+
